7b08d424e6d885663d952bccd80b458de67d20b5
Shirin Zarqaa <shirinzarqaa354@gmail.com>
2025-03-09 16:48:24 +0700

    [FIX] add missing router for delete invoice

--- backend/src/routes/invoiceRoutes.js
+++ backend/src/routes/invoiceRoutes.js
@@ -3,15 +3,10 @@ const router = express.Router();
 const InvoiceController = require('../controllers/invoiceController');
 const authMiddleware = require('../middlewares/authMiddleware');
 
-router.post(
-    '/upload',
-    authMiddleware,               
-    InvoiceController.uploadMiddleware,
-    InvoiceController.uploadInvoice
-);
+router.post('/upload', authMiddleware, InvoiceController.uploadMiddleware,InvoiceController.uploadInvoice);
 
 router.get('/:id', InvoiceController.getInvoiceById);
-
 router.post('/analyze', InvoiceController.analyzeInvoice);
+router.delete('/:id', authMiddleware, InvoiceController.deleteInvoiceById);
 
 module.exports = router;
