31ed3bf0848063d98931585d0b1261909c4f000f
asteriskzie <ester.gracia.10@gmail.com>
2025-05-14 13:54:36 +0700

    [REFACTPR] Adjust pdf decryption strategy to async to match implementation

--- backend/src/strategies/pdfDecryptionStrategy.js
+++ backend/src/strategies/pdfDecryptionStrategy.js
@@ -1,5 +1,6 @@
 class PdfDecryptionStrategy {
-    decrypt() {
+    // eslint-disable-next-line no-unused-vars
+    async decrypt(pdfBuffer, password) {
         throw new Error('decrypt method must be implemented');
     }
 }
